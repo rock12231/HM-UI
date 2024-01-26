a9dd745aa4a06bf3363ad09909e7c63d5d2a5036
Avinash Kumar <37548802+rock12231@users.noreply.github.com>
2024-01-26 16:56:42 +0530

    GET IP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HackpostComponent } from './hackpost/hackpost.component';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HackpostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'HM-UI';
+  constructor(private httpClient: HttpClient) {}
 }
--- src/app/hackpost/hackpost.component.ts
+++ src/app/hackpost/hackpost.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-hackpost',
   standalone: true,
-  imports: [],
+  imports: [ HttpClientModule],
   templateUrl: './hackpost.component.html',
   styleUrl: './hackpost.component.css'
 })
 export class HackpostComponent {
+  title = 'HM-UI';
+  ip: string | undefined;
 
+  constructor(private http: HttpClient) {
+    this.getIPAddress();
+  }
+
+  // Get ip address
+  getIPAddress() {
+    this.http.get('https://api.ipify.org/?format=json').subscribe((res: any) => {
+      this.ip = res.ip;
+      console.log(this.ip);
+    });
+  }
 }
